cb05d1d9ea683644cabd8f1ef92f18c03b705e1a
Ken Nzioka <nzioker@gmail.com>
2026-02-08 21:29:04 +0300

    Fix: Adjusted requirements.txt

--- ecommerce-backend/ecommerce/settings.py
+++ ecommerce-backend/ecommerce/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'django_filters',
-    'drf_yasg',
+    'drf_spectacular',
     
     'users',
     'products',
--- ecommerce-backend/ecommerce/urls.py
+++ ecommerce-backend/ecommerce/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     path('api/v1/', include('products.urls')),
     
     # API Documentation
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
